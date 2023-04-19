a7cf4fe2fcdf4e7562d72ec749032e185a692c54
rladydgn <39542757+rladydgn@users.noreply.github.com>
2023-04-20 08:15:48 +0900

    logs

--- hello/src/main/java/com/springboot/hello/controller/HelloController.java
+++ hello/src/main/java/com/springboot/hello/controller/HelloController.java
@@ -1,13 +1,18 @@
 package com.springboot.hello.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping("/hello")
     public String hello() {
+        LOGGER.info("hello");
         // RequestMapping -> url 매핑
         return "Hello World";
     }
